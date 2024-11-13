f0c018dc3fa4e368021b40b632e86891da333d2e
Adithyan A <deepaadithyan56@gmail.com>
2024-11-13 08:56:19 +0530

    Dynamically show main button in hero to either signin or goto collections

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,6 +1,11 @@
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { signInAction } from "@/lib/actions";
+import { auth, signIn } from "@/auth";
 
 export default async function Home() {
+  const session = await auth();
+
   return (
     <main className="flex h-screen w-full flex-col items-center justify-center gap-5">
       <h1 className="space-y-5 text-center text-4xl font-semibold md:text-5xl">
@@ -13,12 +18,23 @@ export default async function Home() {
         Get started by seeing the collections of curated problems and solutions for different labs.
       </p>
 
-      <Link href={"/collections"} className="relative mt-5 p-[3px]">
-        <div className="absolute inset-0 rounded-lg bg-gradient-to-r from-indigo-500 to-purple-500" />
-        <div className="group relative rounded-[6px] bg-black px-9 py-2.5 text-xl font-semibold text-white transition duration-200 hover:bg-transparent">
-          Lit up borders
-        </div>
-      </Link>
+      {session ? (
+        <Link href={"/collections"} className="relative mt-5 p-[3px]">
+          <div className="absolute inset-0 rounded-lg bg-gradient-to-r from-indigo-500 to-purple-500" />
+          <div className="group relative rounded-[6px] bg-black px-9 py-2.5 text-xl font-semibold text-white transition duration-200 hover:bg-transparent">
+            See collections
+          </div>
+        </Link>
+      ) : (
+        <form action={signInAction} className="relative mt-5 p-[3px]">
+          <Button variant="default" type="submit" className="h-auto p-0">
+            <div className="absolute inset-0 rounded-lg bg-gradient-to-r from-indigo-500 to-purple-500" />
+            <div className="group relative rounded-[6px] bg-black px-9 py-2.5 text-xl font-semibold text-white transition duration-200 hover:bg-transparent">
+              Sign in with Google
+            </div>
+          </Button>
+        </form>
+      )}
     </main>
   );
 }
