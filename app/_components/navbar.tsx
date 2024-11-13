a2eff7e3987ca3160fc5931649bd99f5fd159208
Adithyan A <deepaadithyan56@gmail.com>
2024-11-13 08:54:45 +0530

    Add Logout and Login buttons to navbar and remove back button

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -1,20 +1,30 @@
-import { Suspense } from "react";
 import Link from "next/link";
-import { Skeleton } from "@/components/ui/skeleton";
-import { BackButton } from "./back-button";
+import { LogOutIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { signInAction, signOutAction } from "@/lib/actions";
+import { auth, signIn, signOut } from "@/auth";
 
-export function Navbar() {
+export async function Navbar() {
+  const session = await auth();
+
   return (
     <header className="border-b shadow-sm">
-      <div className="container relative mx-auto flex items-center justify-between px-4 py-4 sm:justify-center">
-        <div className="sm:absolute sm:left-0">
-          <Suspense fallback={<Skeleton className="size-6" />}>
-            <BackButton />
-          </Suspense>
-        </div>
+      <div className="container relative mx-auto flex h-14 items-center justify-between px-4">
         <Link href="/">
           <h1 className="text-2xl font-semibold">CoderCodes</h1>
         </Link>
+
+        {session ? (
+          <form action={signOutAction}>
+            <Button variant="ghost" size="icon" aria-description="Logout Button">
+              <LogOutIcon className="size-6" />
+            </Button>
+          </form>
+        ) : (
+          <form action={signInAction}>
+            <Button variant="default">Login</Button>
+          </form>
+        )}
       </div>
     </header>
   );
