c9eacace345d01c2e747c16fac9c7f512d9e38c8
Adithyan A <deepaadithyan56@gmail.com>
2024-11-15 21:58:48 +0530

    Create a navbar dropdown and move logout option to it

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -1,7 +1,17 @@
 import Link from "next/link";
-import { LogOutIcon } from "lucide-react";
+import { LogOutIcon, UserIcon } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { signInAction, signOutAction } from "@/lib/actions";
+import { getInitials } from "@/lib/utils";
 import { auth } from "@/auth";
 
 export async function Navbar() {
@@ -14,12 +24,29 @@ export async function Navbar() {
           <h1 className="gradient1 gradient_text text-2xl font-semibold transition-all duration-300">CoderCodes</h1>
         </Link>
 
-        {session ? (
-          <form action={signOutAction}>
-            <Button variant="ghost" size="icon" aria-description="Logout Button">
-              <LogOutIcon className="size-6" />
-            </Button>
-          </form>
+        {session && session.user ? (
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Avatar>
+                <AvatarImage src={session.user.image || ""} />
+                <AvatarFallback>
+                  {session.user.name ? getInitials(session.user.name) : <UserIcon className="text-muted-foreground" />}
+                </AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel className="text-yellow-500">My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem asChild className="!p-0 text-yellow-500 focus:bg-yellow-500/10 focus:text-yellow-500">
+                <form action={signOutAction}>
+                  <button type="submit" className="w-ful flex items-center gap-2 px-2 py-1.5">
+                    <LogOutIcon className="size-4" />
+                    Logout
+                  </button>
+                </form>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         ) : (
           <form action={signInAction}>
             <Button variant="default">Login</Button>
