0534279477762f39034fd8a89137b505014311dd
Adithyan A <deepaadithyan56@gmail.com>
2024-11-15 21:04:27 +0530

    Remove Layout from Base Layout and add to Root Layout, So that it is not shown in studio route

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "easymde/dist/easymde.min.css";
-import { Navbar } from "@/app/_components/navbar";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -23,10 +22,7 @@ export const metadata: Metadata = {
 export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} dark antialiased`}>
-        <Navbar />
-        {children}
-      </body>
+      <body className={`${geistSans.variable} ${geistMono.variable} dark antialiased`}>{children}</body>
     </html>
   );
 }
