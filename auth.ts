4157760e070760a46930fa8c842cd70cfce79449
Adithyan A <deepaadithyan56@gmail.com>
2024-12-14 19:12:38 +0530

    Replace SanityDB with MongoDB

--- auth.ts
+++ auth.ts
@@ -1,23 +1,22 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
-import { client } from "@/sanity/lib/client";
-import { USER_BY_GOOGLE_EMAIL_QUERY } from "@/sanity/lib/queries";
-import { writeClient } from "@/sanity/lib/write-client";
+import { connectToMongo } from "@/db/client";
+import User from "@/db/models/user";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
   callbacks: {
     async signIn({ user: { name, email, image } }) {
-      const existingUser = await client.withConfig({ useCdn: false }).fetch(USER_BY_GOOGLE_EMAIL_QUERY, { email });
-      if (!existingUser) await writeClient.create({ _type: "user", name, email, image });
+      await connectToMongo();
+      const existingUser = await User.exists({ email });
+      if (!existingUser) await User.create({ name, email, image });
       return true;
     },
 
     async jwt({ token }) {
+      await connectToMongo();
       if (token.email) {
-        const user = await client
-          .withConfig({ useCdn: false })
-          .fetch(USER_BY_GOOGLE_EMAIL_QUERY, { email: token.email });
+        const user = await User.findOne({ email: token.email }).select("_id");
         token.id = user?._id;
       }
       return token;
