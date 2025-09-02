54e375b53f08353a8d952a09e7b36ae469259b6e
Adithyan A <adithyanthedev@gmail.com>
2025-09-03 00:10:33 +0530

    Style logo component

--- components/logo.tsx
+++ components/logo.tsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 export function Logo() {
   return (
     <Link href="/">
-      <h1 className="gradient1 gradient_text text-2xl font-semibold">CoderCodes</h1>
+      <div className="relative bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 bg-clip-text bg-no-repeat text-transparent">
+        <h1 className="text-xl font-semibold">CoderCodes</h1>
+      </div>
     </Link>
   );
 }
