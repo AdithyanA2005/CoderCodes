dfad285183e54029e13a032b61053e1856551ee4
Adithyan A <adithyanthedev@gmail.com>
2025-09-04 00:05:06 +0530

    Bring back `/` page which is now migrated to payload

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,59 +1,47 @@
-import { headers as getHeaders } from 'next/headers.js'
-import Image from 'next/image'
-import { getPayload } from 'payload'
-import React from 'react'
-import { fileURLToPath } from 'url'
+import { Record } from '@/components/record'
+import { getPayloadClient } from '@/lib/payload-client'
 
-import config from '@/payload.config'
-import './styles.css'
+export default async function Collections() {
+  const payload = await getPayloadClient()
 
-export default async function HomePage() {
-  const headers = await getHeaders()
-  const payloadConfig = await config
-  const payload = await getPayload({ config: payloadConfig })
-  const { user } = await payload.auth({ headers })
-
-  const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
+  // TODO: SETUP PAGINATION
+  const categories = await payload.find({
+    collection: 'categories',
+    select: {
+      title: true,
+      slug: true,
+      description: true,
+    },
+    where: {
+      slug: {
+        exists: true,
+      },
+    },
+    sort: '-updatedAt', // Desending order
+  })
+  // console.log(collections)
 
   return (
-    <div className="home">
-      <div className="content">
-        <picture>
-          <source srcSet="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg" />
-          <Image
-            alt="Payload Logo"
-            height={65}
-            src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg"
-            width={65}
-          />
-        </picture>
-        {!user && <h1>Welcome to your new project.</h1>}
-        {user && <h1>Welcome back, {user.email}</h1>}
-        <div className="links">
-          <a
-            className="admin"
-            href={payloadConfig.routes.admin}
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Go to admin panel
-          </a>
-          <a
-            className="docs"
-            href="https://payloadcms.com/docs"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Documentation
-          </a>
+    <main>
+      <section className="max-w-main mx-auto px-4">
+        <header className="mb-4">
+          <h1 className="text-xl font-semibold text-balance">Lab Collections</h1>
+          <p className="text-muted-foreground mt-1 text-sm text-pretty">
+            Clean and minimal listings of all subjects. Tap a subject to see its programs.
+          </p>
+        </header>
+
+        <div className="flex flex-col gap-3">
+          {categories.docs.map((collection) => (
+            <Record
+              key={`category_card_${collection.id}`}
+              title={collection.title}
+              href={`/categories/${collection.slug}`}
+              description={collection.description || ''}
+            />
+          ))}
         </div>
-      </div>
-      <div className="footer">
-        <p>Update this page by editing</p>
-        <a className="codeLink" href={fileURL}>
-          <code>app/(frontend)/page.tsx</code>
-        </a>
-      </div>
-    </div>
+      </section>
+    </main>
   )
 }
