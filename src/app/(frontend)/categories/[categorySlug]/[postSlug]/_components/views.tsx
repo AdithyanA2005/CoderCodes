f4463dee80f84e13a64952158f4979f930f9fe10
Adithyan A <adithyanthedev@gmail.com>
2025-09-04 04:04:48 +0530

    Bring back `/category/[categorySlug]/[postSlug]/` page which is migrated to payload

---
+++ src/app/(frontend)/categories/[categorySlug]/[postSlug]/_components/views.tsx
@@ -0,0 +1,46 @@
+import { after } from 'next/server'
+import { Skeleton } from '@/components/ui/skeleton'
+import { getPayloadClient } from '@/lib/payload-client'
+
+export async function Views({ slug }: { slug: string }) {
+  const payload = await getPayloadClient()
+
+  const post = (
+    await payload.find({
+      collection: 'posts',
+      where: {
+        slug: {
+          equals: slug,
+        },
+      },
+      limit: 1,
+    })
+  ).docs[0]
+
+  let totalViews = 0
+
+  if (post) {
+    totalViews = post.views || 0
+
+    // Increment views after rendering
+    after(async () => {
+      await payload.update({
+        collection: 'posts', // Replace with your collection name
+        id: post.id,
+        data: {
+          views: totalViews + 1,
+        },
+      })
+    })
+  }
+
+  return (
+    <div className="view_container">
+      <span className="font-black">{`${totalViews + 1} view${totalViews + 1 > 1 ? 's' : ''}`}</span>
+    </div>
+  )
+}
+
+export function ViewsSkeleton() {
+  return <Skeleton className="view_container bg-primary-foreground/30 min-w-24" />
+}
