9ef64eb77a047b0b943f1a6214363ad3984d9c83
Adithyan A <adithyanthedev@gmail.com>
2025-09-03 17:12:16 +0530

    Clean payload blank project

--- sanity/schemaTypes/index.ts
+++
@@ -1,8 +0,0 @@
-import { type SchemaTypeDefinition } from "sanity";
-import { category } from "@/sanity/schemaTypes/category";
-import { post } from "@/sanity/schemaTypes/post";
-import { user } from "@/sanity/schemaTypes/user";
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [user, post, category],
-};
