8b41bdca389a8a6442977d11481319ff1cbf8730
Adithyan A <adithyanthedev@gmail.com>
2025-09-06 21:06:39 +0530

    Update auth.ts so that session also includes user id

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import { upsertUser } from "./lib/actions/payload";
+import { getPayloadClient } from "./lib/payload-client";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
@@ -15,15 +16,23 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     },
 
     async jwt({ token }) {
-      // if (token.email) {
-      //   const user = await User.findOne({ email: token.email }).select("_id");
-      //   token.id = user?._id;
-      // }
+      const payload = await getPayloadClient();
+      if (token.email) {
+        const user = (
+          await payload.find({
+            collection: "users",
+            where: { email: { equals: token.email } },
+          })
+        ).docs[0];
+
+        if (user) token.id = user.id;
+      }
+
       return token;
     },
 
     async session({ session, token }) {
-      // Object.assign(session, { id: token.id });
+      Object.assign(session.user, { id: token.id });
       return session;
     },
   },
